3e48c39b30042fc138b580f0f87c27481bdf71cb
Nathan <nathan.fenimore@gmail.com>
2018-10-17 08:55:20 +1000

    completed fruit_juice

--- fruit_juice.rb
+++ fruit_juice.rb
@@ -22,17 +22,32 @@
 
 class Jug
     def initialize(capacity)
+        @capacity = capacity
+        @juices = {}
+        @total = 0.0
     end
 
     def add(amount, type)
+        capacity_remaining = @capacity - @total
+        amount = capacity_remaining if amount > capacity_remaining
+
+        @juices[type] = amount.to_f
+        @total += amount.to_f
     end
 
     def pour(amount)
+        for type, volume in @juices do
+            @juices[type] = volume - (getConcentration(type) * amount)
+        end
+        @total -= amount
     end
 
     def getTotalAmount
+        @total = @capacity if @total > @capacity
+        @total
     end
 
     def getConcentration(type)
+        @juices[type] ? @juices[type] / @total : 0
     end
 end
