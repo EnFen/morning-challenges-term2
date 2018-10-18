a8e75311f455874121dd7ca691d867cbc08d6609
Nathan <nathan.fenimore@gmail.com>
2018-10-19 09:06:13 +1000

    completed baseball

--- baseball.rb
+++ baseball.rb
@@ -26,15 +26,53 @@
 # For more information on baseball visit here: http://en.wikipedia.org/wiki/Baseball
 #
 class Baseball
+    HITS = {'homerun' => 4, 'triple' => 3, 'double' => 2, 'single' => 1, 'out' => 0}
+
     def initialize
-    end    
-    # A public method accepting the result of an at-bat
-    # @param {string} entry - The result of an at-bat 
-    # Acceptable values: ('single', 'double', 'triple', 'homerun' or 'out') 
-    def addEntry(entry)
+        @score = {home: 0, away: 0}
+
+        # set innings starting values
+        @bases = [0,0,0]  
+        @out = 0
+
+        # away team starts; teams identified by @score.keys
+        @team_index = 1
+        @team_at_bat = @score.keys[@team_index]
+              
     end
-    # A public method returning the current score
-    # Format: "Home: [HOME_SCORE] Away: [AWAY_SCORE]"
+
+    def addEntry(entry)
+        batters_hit = HITS[entry]
+        
+        # increment 'outs' if no result from bat
+        if batters_hit == 0
+            @out += 1
+
+            # when outs reach 3, toggle teams and start new innings 
+            if @out == 3
+                @team_at_bat = @score.keys[@team_index ^= 1]
+                @bases = [0,0,0]
+                @out = 0                
+            end
+
+        # for all other results, bases are taken, and scores counted
+        else
+
+            # batter takes a base...
+            @bases.unshift(1)
+            # bases 'behind' player are left empty
+            (batters_hit - 1).times{@bases.unshift(0)}
+
+            # players that have passed 'home' are scored
+            @score.select { |team, score| 
+                @score[team] += @bases.pop(batters_hit).sum if team == @team_at_bat
+            }
+        end
+
+    end
+
     def to_s
+        "Home: #{@score[:home]} Away: #{@score[:away]}"
     end
 end
+
