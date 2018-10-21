399d77d820397ccda8746e20ea48bea11094bc4c
Nathan <nathan.fenimore@gmail.com>
2018-10-22 08:51:43 +1000

    completed roman numerals

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -23,8 +23,39 @@
 #   M	    1000
 #
 class RomanNumerals
-    def self.fromRoman(number)
+    ROMAN_NUMBERS = {
+    1000 => "M",  
+     900 => "CM",  
+     500 => "D",  
+     400 => "CD",
+     100 => "C",  
+      90 => "XC",  
+      50 => "L",  
+      40 => "XL",  
+      10 => "X",  
+        9 => "IX",  
+        5 => "V",  
+        4 => "IV",  
+        1 => "I",  
+  }
+
+    def self.fromRoman(string)
+        numeral = 0
+        ROMAN_NUMBERS.each do |value, letter| 
+            while string.start_with?(letter)
+                numeral += value
+                string.delete_prefix!(letter)
+            end
+        end
+        return numeral
     end
+
     def self.toRoman(number)
+        roman = ""
+        ROMAN_NUMBERS.each do |value, letter|
+            roman << letter * (number / value)
+            number = number % value
+        end
+        return roman
     end
 end
