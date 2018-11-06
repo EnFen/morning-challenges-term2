7b22719b079439d32c99080508dd20a5abd8ef1b
Nathan <nathan.fenimore@gmail.com>
2018-11-06 13:58:27 +1000

    forum challenge updated

--- forum/app/controllers/questions_controller.rb
+++ forum/app/controllers/questions_controller.rb
@@ -1,9 +1,15 @@
 class QuestionsController < ApplicationController
+        
+    def index
+        redirect_to root_path
+    end
+
     def create
         @question = Question.create(question_params)
         Question.last.nil? ? @question.id = 1 : Question.last.id + 1
         
         redirect_to root_path
+        # redirect_to Question.last
     end
 
     def show
